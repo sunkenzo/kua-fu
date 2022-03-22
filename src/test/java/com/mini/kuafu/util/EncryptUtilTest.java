7471fc992c6f79dc72a419ac73ddc4d7790c8288
kenzo <1171915331@qq.com>
2022-03-22 10:39:05 +0800

    up version

--- src/test/java/com/mini/kuafu/util/EncryptUtilTest.java
+++ src/test/java/com/mini/kuafu/util/EncryptUtilTest.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 /**
  * @author kenzo
@@ -16,5 +17,17 @@ public class EncryptUtilTest {
     @Test
     public void mobilEncrypt() {
         logger.info("{}", EncryptUtil.mobileEncrypt("18201434735"));
+    }
+
+    @Test
+    public void testDuration() {
+        LocalDateTime startTime = TimeHelper.parseDateTime("2021-06-23 00:00:00");
+        LocalDateTime endTime = TimeHelper.parseDateTime("2021-08-23 00:00:00");
+
+        logger.info("days equals: " + startTime.equals(endTime));
+
+        long days = Duration.between(startTime, endTime).toDays();
+        System.out.println("days: " + days);
+        logger.info("days: {}", days);
     }
 }
